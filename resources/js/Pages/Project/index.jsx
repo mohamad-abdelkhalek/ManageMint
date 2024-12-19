eeffbd09282062c20c3edf98febca77cb8b534f8
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-19 16:28:56 +0200

    Added: Projects table structure & styles

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -17,7 +17,35 @@ export default function Index({ auth, projects }) {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-                <pre>{JSON.stringify(projects, undefined, 2)}</pre>
+              <table
+                className="w-full text-sm text-left rtl:text-right
+                text-gray-500 dark:text-gray-400"
+              >
+                <thead
+                  className="text-xs text-gray-700 uppercase bg-gray-50 
+                  dark:bg-gray-700 dark:text-gray-400
+                  border-b-2 border-gray-500"
+                >
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-2">ID</th>
+                    <th className="px-3 py-2">Image</th>
+                    <th className="px-3 py-2">Name</th>
+                    <th className="px-3 py-2">Status</th>
+                    <th className="px-3 py-2">Create Date</th>
+                    <th className="px-3 py-2">Due Date</th>
+                    <th className="px-3 py-2">Created By</th>
+                    <th className="px-3 py-2">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    className="bg-white border-b dark:bg-gray-800
+                  dark:border-gray-700"
+                  >
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
