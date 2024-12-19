c9be09d00fcd838b6ef44a54328506bf3b768554
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-19 17:34:52 +0200

    Updated: Pagination component (Functionality, styles)

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -1,13 +1,23 @@
-import { Link } from "@inertiajs/react"
+import { Link } from "@inertiajs/react";
 
 export default function Pagination({ links }) {
     return (
         <nav className="text-center mt-4">
-            {links.map(link => (
-                <Link>
-                    {link.label}
-                </Link>
+            {links.map((link) => (
+                <Link
+                    preserveScroll
+                    href={link.url || ""}
+                    key={link.label}
+                    className={
+                        "inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
+                        (link.active ? "bg-gray-950 " : "") +
+                        (!link.url
+                            ? "cursor-not-allowed opacity-50"
+                            : "hover:bg-gray-950")
+                    }
+                    dangerouslySetInnerHTML={{ __html: link.label }}
+                ></Link>
             ))}
         </nav>
-    )
+    );
 }
