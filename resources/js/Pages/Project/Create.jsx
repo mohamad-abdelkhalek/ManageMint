47bb3832c609c145caf7e28f544abe1844eb7385
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-22 18:02:37 +0200

    Feat: Added project creation form with useForm handling

--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -1,14 +1,28 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, useForm } from "@inertiajs/react";
 
 export default function Create({ auth }) {
+  const { data, setData, post, errors, reset } = useForm({
+    image: "",
+    name: "",
+    status: "",
+    description: "",
+    due_date: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    post(route("project.create"));
+  };
+
   return (
     <AuthenticatedLayout
       user={auth.user}
       header={
         <div className="flex justify-between items-center">
           <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-          Create new Project
+            Create new Project
           </h2>
         </div>
       }
@@ -19,10 +33,11 @@ export default function Create({ auth }) {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-                <form className="p-4 sm:p-8 bg-white dark:bg-gray-800
-                shadow sm:rounded-lg">
-
-                </form>
+              <form
+                onSubmit={onSubmit}
+                className="p-4 sm:p-8 bg-white dark:bg-gray-800
+                shadow sm:rounded-lg"
+              ></form>
             </div>
           </div>
         </div>
