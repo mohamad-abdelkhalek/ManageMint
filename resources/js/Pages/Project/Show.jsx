539db15ff266baa69fe687b7767641684ec0781b
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-22 20:55:58 +0200

    Updated: Displaying tasks table only if project have tasks

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -85,11 +85,15 @@ export default function Show({ auth, project, tasks, queryParams }) {
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              <TasksTable
-                tasks={tasks}
-                queryParams={queryParams}
-                hideProjectColumn={true}
-              />
+              {tasks && tasks.data && tasks.data.length > 0 ? (
+                <TasksTable
+                  tasks={tasks}
+                  queryParams={queryParams}
+                  hideProjectColumn={true}
+                />
+              ) : (
+                <p>No tasks available for this project.</p>
+              )}
             </div>
           </div>
         </div>
