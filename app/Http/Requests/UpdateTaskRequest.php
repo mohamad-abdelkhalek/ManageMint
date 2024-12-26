47b1d40ee39421f1c199c65ebdbcde28bdc79ee3
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-26 19:18:30 +0200

    Feat: Edit task data

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\ProjectPriority;
+use App\Enums\ProjectStatus;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateTaskRequest extends FormRequest
 {
@@ -11,7 +14,7 @@ class UpdateTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +25,23 @@ class UpdateTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => ["required", "max:255"],
+            "image" => ["nullable", "image"],
+            "description" => ["nullable", "string"],  // Made nullable as description can be empty
+            "due_date" => ["nullable", "date"],  // Optional date field
+            "project_id" => ["required", "exists:projects,id"],
+            "assigned_user_id" => ["required", "exists:users,id"],
+
+            "status" => [
+                "required",
+                Rule::enum(ProjectStatus::class)
+            ], // enum validation
+
+            "priority" => [
+                "required",
+                Rule::enum(ProjectPriority::class)
+            ], // enum validation
+
         ];
     }
 }
