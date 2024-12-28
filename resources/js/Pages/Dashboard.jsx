2f4dff5e7222dcd41b9c516813381beb8e3993fb
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-28 18:49:56 +0200

    Added: Edit button to task page

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -67,7 +67,7 @@ export default function Dashboard({
                 My Active Tasks
               </h3>
               <table
-                className="w-full text-sm text-left rtl:text-right
+                className="mt-3 w-full text-sm text-left rtl:text-right
                 text-gray-500 dark:text-gray-400"
               >
                 <thead
