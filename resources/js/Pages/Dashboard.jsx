61edaf4d711b23582ae099c020f80a1f8ca2fb9f
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-28 18:32:00 +0200

    Updated: My active tasks structure

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
@@ -8,7 +9,7 @@ export default function Dashboard({
   myProgressTasks,
   totalCompletedTasks,
   myCompletedTasks,
-  activeTasks
+  activeTasks,
 }) {
   return (
     <AuthenticatedLayout
@@ -65,24 +66,39 @@ export default function Dashboard({
               <h3 className="text-gray-200 text-xl font-semibold">
                 My Active Tasks
               </h3>
-              <table>
-                <thead>
+              <table
+                className="w-full text-sm text-left rtl:text-right
+                text-gray-500 dark:text-gray-400"
+              >
+                <thead
+                  className="text-xs text-gray-700 uppercase bg-gray-50 
+                  dark:bg-gray-700 dark:text-gray-400
+                  border-b-3 border-gray-500"
+                >
                   <tr>
-                    <th>ID</th>
-                    <th>Project Name</th>
-                    <th>Name</th>
-                    <th>Status</th>
-                    <th>Due Date</th>
+                    <th className="px-3 py-3">ID</th>
+                    <th className="px-3 py-3">Project Name</th>
+                    <th className="px-3 py-3">Name</th>
+                    <th className="px-3 py-3">Status</th>
+                    <th className="px-3 py-3">Due Date</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {activeTasks.data.map(task => (
+                  {activeTasks.data.map((task) => (
                     <tr key={task.id}>
-                      <td>{task.id}</td>
-                      <td>{task.project.name}</td>
-                      <td>{task.name}</td>
-                      <td>{task.status}</td>
-                      <td>{task.due_date}</td>
+                      <td className="px-3 py-2">{task.id}</td>
+                      <td className="px-3 py-2">{task.project.name}</td>
+                      <td className="px-3 py-2">{task.name}</td>
+                      <td className="px-3 py-2">
+                        <span
+                          className={`px-2 py-1 rounded text-nowrap text-white ${
+                            TASK_STATUS_CLASS_MAP[task.status]
+                          }`}
+                        >
+                          {TASK_STATUS_TEXT_MAP[task.status]}
+                        </span>
+                      </td>
+                      <td className="px-3 py-2">{task.due_date}</td>
                     </tr>
                   ))}
                 </tbody>
