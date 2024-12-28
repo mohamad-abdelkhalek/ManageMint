0f6bdacc8fce83acd8182b35090d095ca8678012
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-28 18:40:47 +0200

    Added: Link to task page

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -92,7 +92,11 @@ export default function Dashboard({
                           {task.project.name}
                         </Link>
                       </td>
-                      <td className="px-3 py-2">{task.name}</td>
+                      <td className="px-3 py-2 text-white hover:underline">
+                        <Link href={route("task.show", task.id)}>
+                          {task.name}
+                        </Link>
+                      </td>
                       <td className="px-3 py-2">
                         <span
                           className={`px-2 py-1 rounded text-nowrap text-white ${
